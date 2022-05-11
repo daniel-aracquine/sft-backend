4d0005da2793a6a61e3291ba49b559fb9caf4a7d
daniel-aracquine <sarthakmanoch1103@gmail.com>
2022-05-11 17:51:48 +0530

    u6

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema(
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        lowercase:true
     },
     password:{
         type:String,
